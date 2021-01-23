8614a697c9f195480a5cb5d58bd7200b56b31fd6
Dilettant <dedenev1985@gmail.com>
2021-01-23 17:02:50 +0300

    fix: update brain-even game with brain_game_engine

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,43 +1,32 @@
-#!/usr/bin/env python
-"""Even check game."""
-
+#!/usr/bin/env python3
+"""Even-check game."""
 import random
 
-import prompt
-
 from brain_games.cli import welcome_user
+from brain_games.scripts.brain_game_engine import run_game_engine
+
+purpose_of_the_game = (
+    'Answer "yes" if the number is even, otherwise answer "no".'
+    )
 
 
-def ask_even_game(username):
-    """Offer to player to determine the parity of random number.
+def generate_question():
+    """Return a question for the game."""
+    # limit the random number to two-digits for the convenience of the player
+    number_size_limiter = 99
+    return random.randint(1, number_size_limiter)  # noqa: S311
 
-    Up to three correct answers. Return None in case of wrong answer.
-    """  # noqa: DAR101 DAR201
-    correct_answer = 0
-    print('Answer "yes" if the number is even, otherwise answer "no".')  # noqa: WPS421
-    while correct_answer < 3:
-        # limit the value to two-digit number
-        number = random.randint(1, 99)  # noqa: S311 WPS432
-        print('Question: {0}'.format(number))  # noqa: WPS421
-        answer = prompt.string('Your answer: ')
-        if number % 2 == 0 and answer == 'yes' or number % 2 != 0 and answer == 'no':
-            print('Correct!')  # noqa: WPS421
-            correct_answer += 1
-        else:
-            if number % 2 == 0:  # noqa: WPS513
-                print("'{0}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {1}!".format(answer, username))  # noqa: WPS421
-                return None
-            else:
-                print("'{0}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {1}!".format(answer, username))  # noqa: WPS421
-                return None
 
-    print('Congratulations, {0}!'.format(username))  # noqa: WPS421
+def calculate_correct_answer(question):
+    if question % 2 == 0:
+        return 'yes'
+    return 'no'
 
 
 def main():
-    """Start the even game."""
-    ask_even_game(welcome_user())
+    run_game_engine(welcome_user, purpose_of_the_game, generate_question, calculate_correct_answer)
 
 
 if __name__ == '__main__':
     main()
+
